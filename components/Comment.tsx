292355a8a0551a4d609df541b2c244eae4ae175a
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-29 18:56:44 +0900

    feat: comment page

---
+++ components/Comment.tsx
@@ -0,0 +1,19 @@
+import { CommentType } from '../types/Types'
+
+const Comment: React.FC<CommentType> = ({ id, name, body }) => {
+  return (
+    <li className="mx-10">
+      <p className="">
+        {id}
+        {': '}
+        {body}
+      </p>
+      <p className="text-center mt-3 mb-10">
+        {'by '}
+        {name}
+      </p>
+    </li>
+  )
+}
+
+export default Comment
--- pages/comment-page.tsx
+++ pages/comment-page.tsx
@@ -1,9 +1,28 @@
+import useSWR from 'swr'
+import axios from 'axios'
+import Comment from '../components/Comment'
 import Layout from '../components/Layout'
+import { CommentType } from '../types/Types'
+
+const axiosFetcher = async () => {
+  const result = await axios.get<CommentType[]>(
+    'https://jsonplaceholder.typicode.com/comments/?_limit=10'
+  )
+  return result.data
+}
 
 const CommentPage: React.FC = () => {
+  const { data: comments, error } = useSWR('commentsFetch', axiosFetcher)
+
+  if (error) return <span>Error!</span>
+
   return (
     <Layout title="Comment">
-      <p className="text-4xl">comment page</p>
+      <p className="text-4xl m-10">comment page</p>
+      <ul>
+        {comments &&
+          comments.map((comment) => <Comment key={comment.id} {...comment} />)}
+      </ul>
     </Layout>
   )
 }
